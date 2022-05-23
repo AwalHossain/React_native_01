7cd09ab8317937e5cfc03728b06616d98ba0182f
AwalHossain <waqin21@gmail.com>
2022-05-23 19:33:49 +0600

    start exploring native composit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import { SafeAreaView } from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <>
+  
+    <View style={{backgroundColor:"green", height: 60, padding:20, marginTop:10}}>
+      <Text>Kmai There</Text> 
+      <Text>Kmai There</Text> 
     </View>
+    <View style={{backgroundColor:"blue", height: 60}}>
+    
+    <Text>Come on man</Text>
+    </View>
+
+
+</>
+
   );
 }
 
@@ -14,7 +25,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });
